9fa3b70803cb09aa283685bc8f355008cdedacaa
Shashank1197 <wwwladdu94@gmail.com>
2024-06-27 20:13:38 +0530

    nocss

---
+++ to do list/index.js
@@ -0,0 +1,16 @@
+function addOne(){
+    var task=getElementById("inputitem").value;
+
+    document.getElementById("todo_box").innerHTML +="<div class='box'></div>"+task+"</div><div onclick ='rem(this);'>X</div></div>";
+    document.getElementById("inputitem").value="";
+    
+}
+function rem(elem){
+    var task2=elem.parentElement.innerText.split("\n")[0];
+    document.getElementById("complete").innerHTML +=
+    "<div class ='box'><div>"+task2+"</div><div onclick = 'box(this);'>X</div></div>";
+    elem.parentElement.remove();
+}
+function con(el){
+    el.parentElement.remove();
+}
