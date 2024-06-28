035f220ba1e4d09c6d43c92117778543a0fcb438
Shashank1197 <wwwladdu94@gmail.com>
2024-06-28 10:08:27 +0530

    problem in todo box

--- to do list/index.js
+++ to do list/index.js
@@ -1,5 +1,5 @@
 function addOne(){
-    var task=getElementById("inputitem").value;
+    var task=document.getElementById("inputitem").value;
 
     document.getElementById("todo_box").innerHTML +="<div class='box'></div>"+task+"</div><div onclick ='rem(this);'>X</div></div>";
     document.getElementById("inputitem").value="";
@@ -8,7 +8,7 @@ function addOne(){
 function rem(elem){
     var task2=elem.parentElement.innerText.split("\n")[0];
     document.getElementById("complete").innerHTML +=
-    "<div class ='box'><div>"+task2+"</div><div onclick = 'box(this);'>X</div></div>";
+    "<div class ='box'><div>"+task2+"</div><div onclick='con(this);'>X</div></div>";
     elem.parentElement.remove();
 }
 function con(el){
